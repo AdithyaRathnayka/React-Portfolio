68b9d580a74a911638d9b787e74682cfa6735a7f
AdithyaRathnayaka <adikawmini21@gmail.com>
2023-03-21 00:41:39 +0530

    site added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className='conatiner'>
+          <p>© All rights reserved by Adithya Rathnayaka.</p>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
